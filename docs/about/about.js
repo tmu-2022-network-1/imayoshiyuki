c02951b3572729e55e91709d6b47d85edc949fc3
imayoshi yuki <103159800+imayoshiyuki@users.noreply.github.com>
2022-07-21 22:32:40 +0900

    7/21

--- docs/about/about.js
+++ docs/about/about.js
@@ -1,10 +1,18 @@
-// $('line-syakura').hover(
+// $('.syakura-img').hover(
 //     function() {
- 
+//         this.src = "../images/lineStamps/line_syakura_hover.png";
 //     },
 //     function() {
- 
-//         //マウスカーソルが離れた時の処理
- 
+//         this.src = "../images/lineStamps/line_syakura.png";
 //     }
 // );
+
+// $('.same-img').hover(
+//     function() {
+//         this.src = "../images/lineStamps/line_same_hover.png";
+//     },
+//     function() {
+//         this.src = "../images/lineStamps/line_same.png";
+//     }
+// );
+
